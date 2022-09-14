980e1a02cdd3823dc4d1b0923affefe2edbbd21f
JYG <jojeayun900@gmail.com>
2022-09-14 21:04:57 +0900

    learn to prevent Event

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const loginInput = document.querySelector("#login-form input");
 const loginButton = document.querySelector("#login-form button");
 
-function onLoginBtnClick() {
-  const username = loginInput.value;
-  console.log(username);
+function onLoginSubmit(event) {
+  event.preventDefault();
+  console.log(loginInput.value);
 }
 
-loginButton.addEventListener("click", onLoginBtnClick);
+loginButton.addEventListener("submit", onLoginSubmit);
