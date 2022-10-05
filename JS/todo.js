e047e5d5932d962a5dc6723fa472b8c5ccaf6d26
JYG <jojeayun900@gmail.com>
2022-10-05 19:14:40 +0900

    made a fuction of adding ToDos

--- JS/todo.js
+++ JS/todo.js
@@ -2,10 +2,18 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.getElementById("todo-list");
 
+function painToDo(newTodo){
+    const li = document.createElement("li");
+    const span = document.createElement("span");
+    li.appendChild(span);
+    span.innerText = newTodo;
+    toDoList.appendChild(li);
+}
 function handleToDoSubmit(event) {
     event.preventDefault();
     const newTodo = toDoInput.value;
     toDoInput.value = "";
+    painToDo(newTodo);
   }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
